5cc09726e25eee1f78202ee12efffeb312f7b309
Anupriya Tewari <tewari.anu18@gmail.com>
2023-02-17 18:17:05 -0800

    Removing view model from SurveyList screen

--- app/src/main/java/org/takingroot/assignment/screens/SurveyList.kt
+++ app/src/main/java/org/takingroot/assignment/screens/SurveyList.kt
@@ -2,9 +2,8 @@ package org.takingroot.assignment.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import org.takingroot.assignment.viewmodels.SurveyViewModel
 
 @Composable
-fun SurveyList(viewModel: SurveyViewModel) {
+fun SurveyList() {
     Text(text = "Show the surveys here")
 }
